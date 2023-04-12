afb0b647dd9fa728541d884bd0a9d8680a410a69
anggia05 <anggia.pws@gmail.com>
2023-04-12 11:16:54 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            container: {
+                center: true,
+              },
+            colors: {
+                primary: '#0EC5D7',
+            },
+            fontFamily: {
+                base: ['Inter', 'sans-serif'],
+                nav: ['Raleway', 'sans-serif']
+            },
+            fontSize: {
+                'heading-1': '50px',
+            },
+        },
+    }
+}
