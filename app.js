4fb1de4440354147e9588efdb9e221d5c127919b
anggia05 <anggia.pws@gmail.com>
2023-04-13 08:43:55 +0700

    update

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const buttonMobile = document.querySelector('.mobile-btn');
 const navigation = document.querySelector("nav");
 
@@ -5,16 +6,3 @@ buttonMobile.addEventListener("click", function(event) {
     this.classList.toggle('active');
     navigation.classList.toggle('active');
 })
-
-document.querySelectorAll("a[href*='get-started']").forEach(function(anchor) {
-    anchor.classList.add('button-primary');
-    anchor.addEventListener("click",(event)=> {
-        alert("Membuka popup");
-    });
-});
-
-document.querySelectorAll("li.has-child").forEach(function(anchor) {
-    anchor.querySelector(".caret").addEventListener("click",(event)=> {
-        anchor.classList.toggle('active');
-    });
-});
