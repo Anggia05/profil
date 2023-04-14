f1a9310a91534d9f670e57f307eeb318467bc14a
anggia05 <anggia.pws@gmail.com>
2023-04-14 14:56:33 +0700

    update3

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,18 @@ tailwind.config = {
         extend: {
             container: {
                 center: true,
+                padding: '2rem',
               },
             colors: {
                 primary: '#0EC5D7',
+                nav: '#969696',
+                header: '#F6F9FC',
             },
             fontFamily: {
                 base: ['Inter', 'sans-serif'],
                 nav: ['Raleway', 'sans-serif']
             },
-            fontSize: {
-                'heading-1': '50px',
-            },
+            
         },
     }
 }
